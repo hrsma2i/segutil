e597dc60a8844bbce47155fb4510e93e7fcc2496
hrsma2i <hrs.ma2i@gmail.com>
2021-03-14 21:53:37 +0900

    Skip module names in visualizations.

---
+++ segmentation/visualizations/__init__.py
@@ -0,0 +1,5 @@
+from segmentation.visualizations.colormap import voc_colormap
+from segmentation.visualizations.vis_image import vis_image
+from segmentation.visualizations.vis_semantic_segmentation import (
+    vis_semantic_segmentation,
+)
