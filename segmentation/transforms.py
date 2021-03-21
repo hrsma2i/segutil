f07593aa934e41f48a533358ca9bfbe8751ce723
hrsma2i <hrs.ma2i@gmail.com>
2021-03-21 22:09:44 +0000

    Change background color: 255 -> 0.

--- segmentation/transforms.py
+++ segmentation/transforms.py
@@ -1,4 +1,4 @@
-from typing import Sequence, List
+from typing import Sequence
 import numpy as np
 from PIL import Image
 
@@ -9,22 +9,19 @@ def masks_to_segmap(masks: np.ndarray, class_ids: np.ndarray) -> np.ndarray:
     """Compress binary masks to a segmentatoin map for a particular image
 
     Args:
-        masks        (np.ndarray; {0, 1}^(height, width, #rles_i)): binary masks
+        masks     (np.ndarray; {0, 1}^(height, width, #rles_i)): binary masks
         class_ids (np.ndarray;      C^(#rles_i, )):
             C: {0, 1, ..., #classes-1}
+                0: must be background
             #rles_i: the number of RLE annotations for an image `i`
 
     Returns:
-        segmap (np.ndarray; C'^(height, width)): a segmentation map
-            C': {0, 1, ..., #classes-1, 255}
-                255: background
+        segmap (np.ndarray; C^(height, width)): a segmentation map
     """
-    # `+1` avoids from collision beween class 0 and background
-    segmap = np.max((class_ids + 1) * masks, axis=-1).astype(np.uint8)
+    segmap = np.max(class_ids * masks, axis=-1).astype(np.uint8)
     # segmap: (height, width)
 
-    # `-1` shifts `0` to `255` because segmap is np.uint8
-    return segmap - 1
+    return segmap
 
 
 def segmap_to_pil(
@@ -34,20 +31,15 @@ def segmap_to_pil(
     """[summary]
 
     Args:
-        segmap    (np.ndarray; C'^(height, width)): a segmentation map
-            C': {0, 1, ..., #classes-1, 255}
-                255: background
+        segmap    (np.ndarray; C^(height, width)): a segmentation map
+            C: {0, 1, ..., #classes-1}
+                0: must be background
         all_class_ids (np.ndarray;  C^(#C, )):
-            C:  {0, 1, ..., #classes-1}
 
     Returns:
         Image.Image: [description]
     """
-    background = np.array([255, 255, 255]).reshape(1, -1)
-    palette = np.concatenate(
-        (voc_colormap(all_class_ids), background),
-        axis=0,
-    )
+    palette = voc_colormap(all_class_ids)
 
     img = Image.fromarray(segmap).convert("P")
     img.putpalette(palette.astype(np.uint8))
